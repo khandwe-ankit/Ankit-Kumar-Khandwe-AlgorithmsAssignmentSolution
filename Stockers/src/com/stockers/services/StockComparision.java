43d461bb246f2146ccec821077186c64dfaa0aac
khandwe-ankit <ankit20apr@gmail.com>
2021-12-19 02:08:53 +0530

    formatting

--- Stockers/src/com/stockers/services/StockComparision.java
+++ Stockers/src/com/stockers/services/StockComparision.java
@@ -3,6 +3,7 @@ package com.stockers.services;
 import com.stockers.stocks.Stocks;
 
 public class StockComparision {
+
 	public static int noOfStocksRoseToday(Stocks[] stocksArr) {
 		int noOfStocksRisedToday = 0;
 		for (int i = 0; i < stocksArr.length; i++) {
